66d71b91b256ff2845bc21af22afbc1af1aeb570
Jordan Gittleman <jordandgittleman@gmail.com>
2021-11-01 22:20:22 -0400

    baby's first commit, super small start to pullvember

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+//super simple generic hello world code in c++. baby steps...
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+
+cout << "Hello world!" << endl;
+return 0;
+}
